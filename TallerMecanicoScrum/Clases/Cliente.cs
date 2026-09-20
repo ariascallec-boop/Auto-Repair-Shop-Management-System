ca41031729ae43bcbcc1bf8adecc8c62f8387fbd
Edgar <edgarcristihangarciachambilla@gmail.com>
2026-09-20 00:24:36 -0400

    avance del frontend con comentarios guiese con eso

---
+++ TallerMecanicoScrum/Clases/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace TallerMecanicoScrum.Clases
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = "";
+
+        public string Apellido { get; set; } = "";
+
+        public string CiNit { get; set; } = "";
+
+        public string Telefono { get; set; } = "";
+
+        public string Direccion { get; set; } = "";
+
+        public bool Estado { get; set; } = true;
+    }
+}
